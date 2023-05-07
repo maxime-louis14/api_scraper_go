77f74063be5b0adab3ed1a79f5d4d0add4a0d2ec
maxime <maximelouis321@gmail.com>
2023-05-07 20:06:32 +0200

    fini

--- models/instruction.go
+++ models/instruction.go
@@ -9,7 +9,7 @@ import (
 type Instruction struct {
 	gorm.Model
 	Number      string `json:"number"`
-	Description string `json:"description"`
+	Description string `json:"description" gorm:"type:text"`
 	RecetteID   uint
 	CreatedAt   time.Time
 }
